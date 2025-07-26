cf24e885874c044ade5d657b31d4694def6b2e52
Pranjali Rathi <pranjalirathi05@gmail.com>
2025-07-26 13:10:27 +0530

    Modified route name

--- backend/core/auth.py
+++ backend/core/auth.py
@@ -16,7 +16,7 @@ ALGORITHM = os.getenv("ALGORITHM", "HS256")
 ACCESS_TOKEN_EXPIRE_MINUTE = int(os.getenv("ACCESS_TOKEN_EXPIRE_MINUTE", "30"))
 
 pwd_context = CryptContext(schemes=['bcrypt'], deprecated="auto")
-oauth2_scheme = OAuth2PasswordBearer(tokenUrl="auth/login-oauth2")
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="login-oauth2")
 
 def verify_password(plain_password: str, hashed_password: str) -> bool:
     return pwd_context.verify(plain_password, hashed_password)
